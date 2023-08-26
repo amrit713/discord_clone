af51d287d5a91f2216baa63018c660cb337cd4db
amrit713 <amritghimire741443@gmail.com>
2023-08-26 13:31:20 +0545

    setup for dark-mode

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className=" ">
+    <div className="flex  item-center gap-4 ">
       <Button size="lg">Hello</Button>
+      <ModeToggle />
     </div>
   );
 }
