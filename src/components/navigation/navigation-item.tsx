1c84efd9ccd5f80dfbded697439350fc733f931c
amrit713 <amritghimire741443@gmail.com>
2023-09-07 08:44:15 +0545

    fixed errror in navigation items

--- src/components/navigation/navigation-item.tsx
+++ src/components/navigation/navigation-item.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC } from "react";
+
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 
@@ -7,18 +7,13 @@ import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
 
 interface NavigationItemProps {
-  id: number;
+  id: string;
   imageUrl: string;
   name: string;
 }
 
-export const NavigationItem: FC<NavigationItemProps> = ({
-  id,
-  imageUrl,
-  name,
-}) => {
+export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
   const params = useParams();
-
   const router = useRouter();
 
   const onClick = () => {
@@ -27,22 +22,22 @@ export const NavigationItem: FC<NavigationItemProps> = ({
 
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <button className="group relative flex items-center " onClick={onClick}>
+      <button onClick={onClick} className="group relative flex items-center">
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-            +params?.serverId !== id && "group-hover:h-[20px]",
-            +params?.serverId === id ? "h-[36px]" : "h-[8px]"
+            params?.serverId != id && "group-hover:h-[20px]",
+            params?.serverId == id ? "h-[36px]" : "h-[8px]"
           )}
         />
         <div
           className={cn(
-            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden ",
-            +params.serverId === id &&
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId == id &&
               "bg-primary/10 text-primary rounded-[16px]"
           )}
         >
-          <Image fill objectFit="cover" src={imageUrl} alt="Channel" />
+          <Image objectFit="cover" fill src={imageUrl} alt="Channel" />
         </div>
       </button>
     </ActionTooltip>
